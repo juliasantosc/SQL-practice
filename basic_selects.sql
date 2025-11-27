b6fe5c18bdd03c7ce2447724444c9bdd7ee86a80
juliasantosc <julia.santos.carmo11@gmail.com>
2025-11-27 14:59:11 -0300

    SQL Exercises: SELECT

--- basic_selects.sql
+++ basic_selects.sql
@@ -1,19 +1,17 @@
-/*
-  Basic SELECT Exercises
-  Purpose: Practice simple SQL queries using SELECT, WHERE and ORDER BY.
-*/
 
--- 1. List all customers from the Customers table.
+--SQL Exercises: SELECT
+  
+-- 1. List all customers from the Customers table
 SELECT * FROM customers;
 
--- 2. Show only the name and email of the customers.
+-- 2. Show only the name and email of the customers
 SELECT name, email FROM customers;
 
--- 3. List all products with price greater than 50.
+-- 3. List all products with price greater than 50
 SELECT * FROM products WHERE price > 50;
 
--- 4. Show all orders made in 2024.
+-- 4. Show all orders made in 2024
 SELECT * FROM orders WHERE YEAR(order_date) = 2024;
 
--- 5. List employees ordered by name.
+-- 5. List employees ordered by name
 SELECT * FROM employees ORDER BY name ASC;
