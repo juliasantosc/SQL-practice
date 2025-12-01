5593c2a51693b7822fb0c08a72c0afca1d88e900
juliasantosc <julia.santos.carmo11@gmail.com>
2025-12-01 15:09:54 -0300

    SQL Exercises: SELECT

--- basic_selects.sql
+++ basic_selects.sql
@@ -2,16 +2,24 @@
 --SQL Exercises: SELECT
   
 -- 1. List all customers from the Customers table
-SELECT * FROM customers
+SELECT * 
+FROM customers
 
 -- 2. Show only the name and email of the customers
-SELECT name, email FROM customers
+SELECT name, email 
+FROM customers
 
 -- 3. List all products with price greater than 50
-SELECT * FROM products WHERE price > 50
+SELECT * 
+FROM products 
+WHERE price > 50
 
 -- 4. Show all orders made in 2024
-SELECT * FROM orders WHERE YEAR(order_date) = 2024
+SELECT * 
+FROM orders 
+WHERE YEAR(order_date) = 2024
 
 -- 5. List employees ordered by name
-SELECT * FROM employees ORDER BY name ASC
+SELECT * 
+FROM employees 
+ORDER BY name ASC
